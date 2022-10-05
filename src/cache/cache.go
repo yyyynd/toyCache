0ef9d54169b0dbb8539a50256c836c35d4c59299
yyyyynd <menghy0523@qq.com>
2022-10-05 20:50:07 +0800

    new func

--- src/cache/cache.go
+++ src/cache/cache.go
@@ -3,34 +3,36 @@ package cache
 import (
 	"errors"
 	"runtime"
+	"sync"
 )
 
 type Cache interface {
-	Add(key string, value interface{})
-	Get(key string)(value interface{}, ok bool)
-	Remove(key string) (removeKey string, removeValue interface{})
-	CacheSize() int64
-	MaxCacheNum() int64
-	RemoveOldest() (removeKey string, removeValue interface{})
+	add(key string, value interface{})
+	get(key string)(value interface{}, ok bool)
+	remove(key string) (removeKey string, removeValue interface{})
+	currentEntries() int64
+	maxCacheNum() int64
+	removeOldest() (removeKey string, removeValue interface{})
+	modifyMaxEntries(newMaxNum int64)
 }
 
 var cacheModeMap map[string]func(int64) Cache
-var CacheController Cache
+var controller Cache
+var mu sync.Mutex
 const defaultMaxCacheNum = 5000
 
 func init() {
 	cacheModeMap  = make(map[string]func(int64) Cache)
 	cacheModeMap["LRU"] = NewLRUCache
 	cacheModeMap["LFU"] = NewLFUCache
-	CacheController = NewLRUCache(defaultMaxCacheNum)
+	controller = NewLRUCache(defaultMaxCacheNum)
 }
-
-//func SpecifyCacheSize(size int64) (int64, error){
-//
-//}
 
 //SetCacheMode change cache mode
 func SetCacheMode(modeName string, maxCacheNum int64) error {
+	mu.Lock()
+	defer mu.Unlock()
+
 	if modeName == "" {
 		return errors.New("mode name is nil")
 	}else if maxCacheNum <= 0{
@@ -38,39 +40,91 @@ func SetCacheMode(modeName string, maxCacheNum int64) error {
 	} else if _, ok := cacheModeMap[modeName]; !ok {
 		return errors.New("this mode not existed")
 	}
-	CacheController = cacheDataTransfer(cacheModeMap[modeName](maxCacheNum))
+	controller = cacheDataTransfer(cacheModeMap[modeName](maxCacheNum))
 	runtime.GC()
 	return nil
 }
 
-func AddCacheMode(modeName string, f func(int64) Cache) error{
-	if modeName == ""{
-		return errors.New("mode name is nil")
-	}else if f == nil {
-		return errors.New("f is nil")
-	}else if _, ok := cacheModeMap[modeName]; ok {
-		return errors.New("this mode existed")
+func Add(key string, value interface{}) {
+	mu.Lock()
+	defer mu.Unlock()
+	controller.add(key, value)
+}
+
+func Get(key string) (value interface{}, ok bool){
+	mu.Lock()
+	defer mu.Unlock()
+	value, ok = controller.get(key)
+	return
+}
+
+func Remove(key string) (removeKey string, removeValue interface{}) {
+	mu.Lock()
+	defer mu.Unlock()
+	removeKey, removeValue = controller.remove(key)
+	return
+}
+
+func MaxCacheNum() int64 {
+	mu.Lock()
+	defer mu.Unlock()
+	return controller.maxCacheNum()
+}
+
+func CurrentCacheNum() int64 {
+	mu.Lock()
+	defer mu.Unlock()
+	return controller.currentEntries()
+}
+
+func RemoveOldest() (removeKey string, removeValue interface{}) {
+	mu.Lock()
+	defer mu.Unlock()
+	removeKey, removeValue = controller.removeOldest()
+	return
+}
+
+func ModifyMaxEntries(newMaxNum int64)  {
+	mu.Lock()
+	defer mu.Unlock()
+	controller.modifyMaxEntries(newMaxNum)
+}
+
+func RemoveAll()  {
+	mu.Lock()
+	defer mu.Unlock()
+	for i := controller.currentEntries(); i > 0; i-- {
+		controller.removeOldest()
 	}
-
-	cacheModeMap[modeName] = f
-	return nil
 }
 
-func ModifyExistCacheMode() error {
-	return nil
-}
+//func AddCacheMode(modeName string, f func(int64) Cache) error{
+//	if modeName == ""{
+//		return errors.New("mode name is nil")
+//	}else if f == nil {
+//		return errors.New("f is nil")
+//	}else if _, ok := cacheModeMap[modeName]; ok {
+//		return errors.New("this mode existed")
+//	}
+//
+//	cacheModeMap[modeName] = f
+//	return nil
+//}
+//
+//func ModifyExistCacheMode() error {
+//	return nil
+//}
 
 func cacheDataTransfer(newCache Cache) Cache{
 	//first check weather need delete
-	removeNum := CacheController.CacheSize() - newCache.MaxCacheNum()
+	removeNum := controller.currentEntries() - newCache.maxCacheNum()
 	for ; removeNum > 0; removeNum-- {
-		CacheController.RemoveOldest()
+		controller.removeOldest()
 	}
-	for i := CacheController.CacheSize(); i > 0; i-- {
-		key, value := CacheController.RemoveOldest()
-		newCache.Add(key, value)
+	for i := controller.currentEntries(); i > 0; i-- {
+		key, value := controller.removeOldest()
+		newCache.add(key, value)
 	}
-
 	return newCache
 }
 
